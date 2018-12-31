36cf12915572fed6498c77b85ddca54badb0024a
Rusty Brooks <rbrooks@alienvault.com>
2018-12-31 00:25:37 -0600

    Move app.py to api.py and supply skeleton function

---
+++ src/api/api.py
@@ -0,0 +1,29 @@
+import appenlight_client.ext.flask as appenlight
+from collections import defaultdict
+import copy
+from flask import Flask, request, render_template, request
+# import json
+import logging
+import os
+
+from lib import api_framework, client, config
+from lib.api_framework import api_register, Api, HttpResponse, api_bool
+from flask_cors import CORS
+
+root = os.path.join(os.path.dirname(__file__))
+
+logger = logging.getLogger(__name__)
+app = Flask('cadcam-api', template_folder=os.path.join(root, 'templates'), static_folder=os.path.join(root, 'static'))
+CORS(app)
+
+
+
+
+@api_register(None, require_login=False)
+class TestApi(Api):
+    @classmethod
+    def index(cls):
+	return "hi"
+
+
+api_framework.app_class_proxy(app, '', 'api/test', TestApi())
